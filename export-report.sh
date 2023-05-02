c4b0313f07c31c65f6f3abeecb175c89e203cf6c
Quentin Vignier <qvignier@igraal.com>
2023-05-02 15:07:57 +0200

    Add reporting path to send to codeclimate

--- export-report.sh
+++ export-report.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export CODACY_PROJECT_TOKEN=baf947a3de8641cfa4405f6997109e76
-curl -Ls https://coverage.codacy.com/get.sh | bash
+curl -Ls https://coverage.codacy.com/get.sh report -r tests-report/build/coverage/clover/clover.xml | bash
